f8d86154bb324967abd3183ea2d260f7c38e1f3b
Brandon Corbett <bmcorbet@valdosta.edu>
2018-04-04 22:46:01 -0400

    added lazy instantiation example, re-organizing directories

---
+++ Singleton-Pattern/Singleton-Lazy-Instantiation/Singleton.java
@@ -0,0 +1,23 @@
+public class Singleton {
+   private static Singleton instance = null;
+   
+   private Singleton() {
+      // Exists only to prevent instantiation
+   }
+   
+   // Lazy Instantiation b/c Singleton instance is only
+   // made once it needs to be made. This way it doesn't
+   // occupy memory until the program creates it. 
+   
+   public static Singleton getInstance() {
+      if(instance == null) {
+         instance = new Singleton();
+      }
+      
+      return instance;
+   }
+   
+   public String doSomething() {
+      System.out.println("I'm a Singleton.");
+   }
+}
